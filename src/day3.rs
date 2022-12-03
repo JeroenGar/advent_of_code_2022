c144406eda71674e3ff4e21c0ff9cf684b539129
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-12-03 12:21:15 +0100

    day 3

--- src/day3.rs
+++ src/day3.rs
@@ -5,11 +5,10 @@ use aoc2022::parse_to_vec;
 
 fn main() {
     let input = fs::read_to_string("input/2022/day3.txt").expect("Could not read file");
-
     let rucksacks: Vec<Rucksack> = parse_to_vec(&input, "\n").unwrap();
 
     let sum_misplaced_items = rucksacks.iter()
-        .map(|r| r.item_in_c1_and_c2.unwrap())
+        .map(|r| r.misplaced_item.unwrap())
         .sum::<u32>();
 
     println!("Part 1: {}", sum_misplaced_items);
@@ -24,7 +23,7 @@ fn main() {
 
 struct Rucksack {
     pub item_present: [bool; 53],
-    pub item_in_c1_and_c2: Option<u32>
+    pub misplaced_item: Option<u32>,
 }
 
 impl FromStr for Rucksack {
@@ -34,23 +33,23 @@ impl FromStr for Rucksack {
         if s.len() == 0 {
             return Err(());
         }
-        let (s1, s2) = s.split_at(s.len()/2);
-
-        let mut item_in_c1 = [false; 53];
-        let mut item_in_c2 = [false; 53];
+        let (s1, s2) = s.split_at(s.len() / 2);
+        let (mut item_in_c1, mut item_in_c2) = ([false; 53], [false; 53]);
 
         s1.chars().for_each(|c| item_in_c1[(item_value_mapper(c))] = true);
         s2.chars().for_each(|c| item_in_c2[(item_value_mapper(c))] = true);
 
-        let item_in_c1_and_c2 = item_in_c1.iter().zip(item_in_c2.iter()).zip(0..53)
-            .find(|((a,b), i)| **a && **b)
+        let misplaced_item = item_in_c1.iter()
+            .zip(item_in_c2.iter())
+            .zip(0..53)
+            .find(|((a, b), i)| **a && **b)
             .map(|(_, i)| i);
 
         let item_present = item_in_c1.iter().zip(item_in_c2.iter())
             .map(|(a, b)| *a || *b)
             .collect::<Vec<bool>>().try_into().unwrap();
 
-        Ok(Rucksack { item_present, item_in_c1_and_c2})
+        Ok(Rucksack { item_present, misplaced_item })
     }
 }
 
@@ -65,7 +64,7 @@ fn item_value_mapper(c: char) -> usize {
     }
 }
 
-fn find_badge(r1: &Rucksack, r2: &Rucksack, r3: &Rucksack) -> Result<u32,()> {
+fn find_badge(r1: &Rucksack, r2: &Rucksack, r3: &Rucksack) -> Result<u32, ()> {
     for i in 0..53 {
         if r1.item_present[i] && r2.item_present[i] && r3.item_present[i] {
             return Ok((i) as u32);
