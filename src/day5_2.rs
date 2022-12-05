c4098d7e06d38612a7f06064f860ce3997c23ac3
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-12-05 19:16:45 +0100

    day 5_2

--- src/day5_2.rs
+++ src/day5_2.rs
@@ -7,16 +7,18 @@ use aoc2022::parse_to_vec;
 
 pub fn main() {
     let start = Instant::now();
-    let input = std::fs::read_to_string("input/2022/day5_88MB.txt").unwrap();
+    let input = std::fs::read_to_string("input/2022/day5_210MB.txt").unwrap();
     let mut input_split = input.split("\n\n");
 
     let crate_stacks_input = input_split.next().unwrap();
     let crane_operations = input_split.next().unwrap();
 
     let mut crate_stacks_lines = crate_stacks_input.split('\n').rev().skip(1).collect::<Vec<&str>>();
-    let crate_char_indices: [usize; 9] = [1, 5, 9, 13, 17, 21, 25, 29, 33]; //indices of the crate characters in the lines
 
-    let crate_stacks = (0..9).map(|i| {
+    let n_crates = (crate_stacks_lines[0].len() + 1) / 4;
+    let crate_char_indices = (0..n_crates).map(|i| 1 + (4* i)).collect::<Vec<usize>>();
+
+    let crate_stacks = (0..n_crates).map(|i| {
         let crates = crate_stacks_lines.iter()
             .map(|line| line.chars().nth(crate_char_indices[i]).unwrap())
             .filter(|c| !c.is_whitespace())
@@ -34,47 +36,42 @@ pub fn main() {
     let start_part_1 = Instant::now();
 
     // the final positions of chars we have to print
-    let mut positions_1 = (0..9).map(|i| Position::new(i, 0)).collect::<Vec<Position>>();
+    let mut positions_1 = (0..n_crates).map(|i| Position::new(i, 0)).collect::<Vec<Position>>();
 
     //Do the crane ops in reverse
     reversed_flipped_crane_ops.iter().for_each(|op| {
         positions_1.iter_mut().for_each(|p| {
-            p.traceback_op(op, true);
+            p.do_operation(op, true);
         });
     });
 
     let dur_part_1 = start_part_1.elapsed();
 
-    let result_1 : String = positions_1.iter()
-        .map(|p| {
-            let crate_stack = &crate_stacks[p.stack_index];
-            let crate_char = crate_stack.crates[crate_stack.crates.len() - 1 - p.n_crates_on_top];
-            crate_char
-        }).collect();
+    let find_chars = |pos : Vec<Position>| -> String {
+        pos.iter()
+            .map(|p| {
+                let crate_stack = &crate_stacks[p.stack_index];
+                let crate_char = crate_stack.crates[crate_stack.crates.len() - 1 - p.n_crates_on_top];
+                crate_char
+            }).collect()
+    };
 
-    println!("Part 1: {} in {}ms", result_1, dur_part_1.as_millis());
+    println!("Part 1: {} in {}ms", find_chars(positions_1), dur_part_1.as_millis());
 
     let start_part_2 = Instant::now();
 
-    let mut positions_2 = (0..9).map(|i| Position::new(i, 0)).collect::<Vec<Position>>();
+    let mut positions_2 = (0..n_crates).map(|i| Position::new(i, 0)).collect::<Vec<Position>>();
 
     //Do the crane ops in reverse
     reversed_flipped_crane_ops.iter().for_each(|op| {
         positions_2.iter_mut().for_each(|p| {
-            p.traceback_op(op, false);
+            p.do_operation(op, false);
         });
     });
 
     let dur_part_2 = start_part_2.elapsed();
 
-    let result_2 : String = positions_2.iter()
-        .map(|p| {
-            let crate_stack = &crate_stacks[p.stack_index];
-            let crate_char = crate_stack.crates[crate_stack.crates.len() - 1 - p.n_crates_on_top];
-            crate_char
-        }).collect();
-
-    println!("Part 2: {} in {}ms", result_2, dur_part_2.as_millis());
+    println!("Part 2: {} in {}ms", find_chars(positions_2), dur_part_2.as_millis());
 }
 
 #[derive(Clone)]
@@ -124,7 +121,7 @@ impl Position {
         Self { stack_index, n_crates_on_top }
     }
 
-    pub fn traceback_op(&mut self, op: &CraneOp, reverse: bool) {
+    pub fn do_operation(&mut self, op: &CraneOp, reverse: bool) {
         if op.from == self.stack_index {
             //Crates are being moved from this stack
             if self.n_crates_on_top < op.n_crates {
