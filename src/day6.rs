f3666408b6d61ca54539bc4e935d5ecae0f5c62e
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-12-06 15:26:12 +0100

    day 6

--- src/day6.rs
+++ src/day6.rs
@@ -5,11 +5,12 @@ use std::time::Instant;
 use itertools::Itertools;
 
 fn main() {
-    let input = fs::read_to_string(Path::new("input/2022/day6.txt")).unwrap();
+    let input = fs::read_to_string("/Users/jern/Downloads/aoc22d6xxl.txt").unwrap();
+    //let input = fs::read_to_string(Path::new("input/2022/day6.txt")).unwrap();
     let char_vec = input.chars().filter(|c| c.is_alphabetic()).collect::<Vec<char>>();
     let start = Instant::now();
-    println!("Part 1: {}", find_marker_efficient(&char_vec, 4));
-    println!("Part 2: {}", find_marker_efficient(&char_vec, 14));
+    println!("Part 1: {}", find_marker_multithreaded(&char_vec, 4, 8));
+    println!("Part 2: {}", find_marker_multithreaded(&char_vec, 14, 8));
     println!("Time: {}ms", start.elapsed().as_millis());
 }
 
@@ -52,4 +53,54 @@ fn find_marker_efficient(input: &Vec<char>, n_unique_chars: usize) -> usize{
         marker_index += 1;
     }
     panic!("No substring with {} unique characters found", n_unique_chars);
+}
+
+fn find_marker_more_efficient(input: &Vec<char>, start: usize, stop: usize, n_unique_chars: usize) -> Option<usize>{
+    let mut last_seen = [None; 26];
+    let mut potential_start = 0;
+    for i in start..stop{
+        let c = input[i];
+        let index = c as usize - 'a' as usize;
+        let prev_last_seen = last_seen[index];
+        match prev_last_seen{
+            None => (),
+            Some(prev_i) => {
+                if i < prev_i + n_unique_chars{
+                    //This characters makes the substring not unique
+                    potential_start = usize::max(potential_start, prev_i);
+                }
+            }
+        }
+        last_seen[index] = Some(i);
+        if i == potential_start + n_unique_chars{
+            return Some(potential_start + n_unique_chars + 1);
+        }
+    }
+    None
+}
+
+
+fn find_marker_multithreaded(input: &Vec<char>, n_unique_chars: usize, n_threads: usize) -> usize{
+    let n_chars = input.len();
+    let overlap = n_unique_chars - 1;
+    let indices = (0..n_threads).map(|i| {
+        let start = (i * (n_chars) / n_threads);
+        let end = ((i + 1) * (n_chars) / n_threads) + overlap;
+        (usize::max(0,start), usize::min(end,n_chars))
+    }).collect::<Vec<(usize, usize)>>();
+
+    let mut results = (0..n_threads).map(|_| None).collect::<Vec<Option<usize>>>();
+
+    rayon::scope(|s| {
+        let mut results_iter_mut = results.iter_mut();
+        for i in 0..n_threads {
+            let (start,end) = indices[i];
+            let mut result_slice = results_iter_mut.next().unwrap();
+            s.spawn(move |_| {
+                *result_slice = find_marker_more_efficient(input, start, end, n_unique_chars);
+            });
+        }
+    });
+
+    results.into_iter().filter_map(|x| x).min().unwrap()
 }
