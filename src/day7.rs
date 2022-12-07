5af2815fe88d7810ebce0554b53da29d229fef90
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-12-07 11:08:09 +0100

    day 7

--- src/day7.rs
+++ src/day7.rs
@@ -1,6 +1,6 @@
 use std::slice::Iter;
 
-fn main(){
+fn main() {
     let input = std::fs::read_to_string("input/2022/day7.txt").unwrap();
     let lines = input.split('\n').collect::<Vec<&str>>();
     let start = std::time::Instant::now();
@@ -11,15 +11,16 @@ fn main(){
     let mut all_dirs = vec![];
     let root_dir = parse_dir(&mut lines_iter, &mut all_dirs);
 
-    let part_1 = all_dirs.iter().filter(|d| **d <= 100_000).sum::<usize>();
+    let part_1 = all_dirs.iter()
+        .filter(|d| **d <= 100_000).sum::<usize>();
 
-    let extra_space_required = 30_000_000 - (70_000_000 - root_dir);
+    let space_required = 30_000_000 - (70_000_000 - root_dir);
     let part_2 = all_dirs.iter()
-        .filter(|d| **d >= extra_space_required)
+        .filter(|d| **d >= space_required)
         .min().unwrap();
     println!("Time: {}us", start.elapsed().as_micros());
 
-    println!("Part 1: {}",  part_1);
+    println!("Part 1: {}", part_1);
     println!("Part 2: {}", part_2);
 }
 
@@ -28,32 +29,24 @@ pub fn parse_dir(line_iter: &mut Iter<&str>, dirs: &mut Vec<usize>) -> usize {
     let mut total_file_size = 0;
     loop {
         let line = line_iter.next();
-        if line == None {
-            break; //go back up
+        if line == None || line.unwrap().is_empty() {
+            break; //go back to parent dir
         }
         let mut line_splitter = line.unwrap().split(' ');
-        let split_1 = line_splitter.next().unwrap();
-        if let Ok(file_size) = split_1.parse::<usize>() {
+        let first_wrd = line_splitter.next().unwrap();
+        if let Ok(file_size) = first_wrd.parse::<usize>() {
             total_file_size += file_size; //file detected
-        }
-        else if !split_1.is_empty() && split_1.chars().nth(0).unwrap() == '$' {
-            if line_splitter.next().unwrap() == "cd" {
-                if line_splitter.next().unwrap() == ".." {
-                    break; //go back up
+        } else if first_wrd.starts_with("dir") {
+            continue; //directory detected, ignore
+        } else if first_wrd.chars().nth(0).unwrap() == '$' {
+            match line_splitter.next().unwrap() {
+                "cd" => match line_splitter.next().unwrap() {
+                    ".." => break, //go back to parent dir
+                    _ => total_file_size += parse_dir(line_iter, dirs), // go into dir
                 }
-                else {
-                    // go into directory
-                    total_file_size += parse_dir(line_iter, dirs);
-                }
+                _ => panic!("Unexpected command"),
             }
-            else {
-                panic!("Unexpected command");
-            }
-        }
-        else if split_1 != "dir" && !split_1.is_empty(){
-            //skip dir lines and empty lines
-            panic!("Unexpected command");
-        }
+        } else { panic!("Unexpected command"); }
     }
     dirs.push(total_file_size);
     total_file_size
