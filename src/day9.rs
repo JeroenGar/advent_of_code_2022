efc287c5f19b7bb0caa913da06f61bc69a2bcd36
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-12-09 13:57:10 +0100

    day 9

--- src/day9.rs
+++ src/day9.rs
@@ -6,8 +6,8 @@ const INPUT: &str = include_str!("../input/2022/day9.txt");
 
 fn main(){
     let head_actions  = parse_to_vec::<Action>(INPUT, "\n").unwrap();
-    let mut rope_1 = vec![RopeSegment {x:0,y:0}; 2];
-    let mut rope_2 = vec![RopeSegment {x:0,y:0}; 10];
+    let mut rope_1 = vec![RopeSegment(0,0); 2];
+    let mut rope_2 = vec![RopeSegment(0,0); 10];
 
     println!("Part 1: {}", simulate(&mut rope_1, &head_actions));
     println!("Part 2: {}", simulate(&mut rope_2, &head_actions));
@@ -18,7 +18,7 @@ fn simulate(rope: &mut Vec<RopeSegment>, head_actions: &Vec<Action>) -> usize {
     let mut tail_pos_set = HashSet::new();
     for action in head_actions {
         for _ in 0..action.dist{
-            tail_pos_set.insert(rope[rope.len()-1].pos());
+            tail_pos_set.insert(rope[rope.len()-1].clone());
             let mut rope_iter = rope.iter_mut();
             let mut prev_segment = rope_iter.next().unwrap();
             prev_segment.move_in_dir(&action.dir);
@@ -31,10 +31,27 @@ fn simulate(rope: &mut Vec<RopeSegment>, head_actions: &Vec<Action>) -> usize {
     tail_pos_set.len()
 }
 
-#[derive(Debug, Clone)]
-struct RopeSegment {
-    x: i32,
-    y: i32,
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct RopeSegment(i32,i32);
+
+impl RopeSegment {
+    fn move_in_dir(&mut self, dir: &(i32,i32)){
+        self.0 += dir.0;
+        self.1 += dir.1;
+    }
+
+    fn react(&mut self, prev: &RopeSegment){
+        let (dx,dy) = ((prev.0 - self.0), (prev.1 - self.1));
+        if dx.abs() > 1 || dy.abs() > 1 {
+            //not (diagonally) adjacent or overlapping, segment needs to react
+            if dx.abs() > 0 {
+                self.0 += dx.signum();
+            }
+            if dy.abs() > 0 {
+                self.1 += dy.signum();
+            }
+        }
+    }
 }
 
 struct Action {
@@ -56,29 +73,5 @@ impl FromStr for Action {
         };
         let dist = split.next().unwrap().parse::<usize>().unwrap();
         Ok(Action {dir, dist})
-    }
-}
-
-impl RopeSegment {
-    fn move_in_dir(&mut self, dir: &(i32,i32)){
-        self.x += dir.0;
-        self.y += dir.1;
-    }
-
-    fn react(&mut self, head: &RopeSegment){
-        let (dx,dy) = ((head.x - self.x), (head.y - self.y));
-        if dx.abs() > 1 || dy.abs() > 1 {
-            //not (diagonally) adjacent or overlapping, needs to react
-            if dx.abs() > 0 {
-                self.x += dx.signum();
-            }
-            if dy.abs() > 0 {
-                self.y += dy.signum();
-            }
-        }
-    }
-
-    fn pos(&self) -> (i32, i32){
-        (self.x, self.y)
     }
 }
