e2375ebcf5ef76c2fdf5079c61bca27ac93a7592
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-12-17 20:00:36 +0100

    day17

--- src/day17.rs
+++ src/day17.rs
@@ -10,13 +10,14 @@ const GAS_PATTERN: &str = include_str!("../input/2022/day17.txt");
 const SHAFT_WIDTH: usize = 7;
 
 fn main() {
+    let start = std::time::Instant::now();
     let rock_types = parse_to_vec::<RockType>(ROCKS, "\n\n").unwrap();
     let gas_pattern = GAS_PATTERN.trim().chars().collect_vec();
 
     let part_1 = {
-        let mut shaft = vec![];
-        simulate(&mut shaft, &rock_types, &mut 0, &gas_pattern, &mut 0, 2022);
-        shaft.len()
+        let mut shaft = Shaft::new(&rock_types, &gas_pattern);
+        shaft.simulate(2022);
+        shaft.height()
     };
 
     println!("Part 1: {}", part_1);
@@ -24,18 +25,18 @@ fn main() {
     let part_2 = {
         //search for a two iterations where the shaft and rocks/gas pattern are in the same state
         let ((cycle_start, height_start), (cycle_end, height_end)) = {
-            let (mut shaft, mut rock_index, mut gas_index) = (vec![], 0, 0);
-            let mut state_map: HashMap<SimState, (usize, usize)> = HashMap::new();
+            let mut shaft = Shaft::new(&rock_types, &gas_pattern);
+            let mut state_map= HashMap::new();
 
             let mut cycle = None;
             for i in 0..1_000_000_000_000 {
-                simulate(&mut shaft, &rock_types, &mut rock_index, &gas_pattern, &mut gas_index, 1);
-                let state = SimState::new(&shaft, gas_index, rock_index);
+                shaft.simulate(1);
+                let state = (shaft.minimal_shape(), shaft.gas_index, shaft.rock_index);
                 if state_map.contains_key(&state) {
-                    cycle = Some((state_map[&state], (i, shaft.len())));
+                    cycle = Some((state_map[&state], (i, shaft.height())));
                     break;
                 }
-                state_map.insert(state, (i, shaft.len()));
+                state_map.insert(state, (i, shaft.height()));
             }
             cycle.unwrap()
         };
@@ -47,94 +48,18 @@ fn main() {
 
         //Determine which height the remainder contributes
         let remaining_height = {
-            let (mut shaft, mut rock_index, mut gas_index) = (vec![], 0, 0);
-            simulate(&mut shaft, &rock_types, &mut rock_index, &gas_pattern, &mut gas_index, cycle_end + remainder);
-            shaft.len() - height_end
+            let mut shaft = Shaft::new(&rock_types, &gas_pattern);
+            shaft.simulate(cycle_end + remainder);
+            shaft.height() - height_end
         };
         //Add all of them up to get the total height
         height_start + (height_end - height_start) * n_cycles + remaining_height
     };
 
     println!("Part 2: {:?}", part_2);
+    println!("Time: {:?}", start.elapsed());
 }
 
-fn simulate(shaft: &mut Vec<[bool; SHAFT_WIDTH]>, rock_types: &Vec<RockType>, rock_type_index: &mut usize, gas_pattern: &Vec<char>, gas_index: &mut usize, n_rocks: usize) {
-    for _ in 0..n_rocks {
-        let rock_type = &rock_types[*rock_type_index];
-        *rock_type_index = (*rock_type_index + 1) % rock_types.len();
-        let (mut pos, mut mode) = ((2, shaft.len() + 3), Mode::Gas); //start state
-
-        loop {
-            mode = match mode {
-                Mode::Gas => {
-                    let gas = &gas_pattern[*gas_index];
-                    *gas_index = (*gas_index + 1) % gas_pattern.len();
-                    let new_pos = match gas {
-                        '>' => {
-                            match SHAFT_WIDTH > pos.0 + rock_type.width as usize {
-                                true => (pos.0 + 1, pos.1),
-                                false => (pos.0, pos.1) //Rock is against the right wall
-                            }
-                        }
-                        '<' => {
-                            match pos.0 > 0 {
-                                true => (pos.0 - 1, pos.1),
-                                false => (pos.0, pos.1) //Rock is against the left wall
-                            }
-                        }
-                        _ => { panic!("Invalid gas character: {}", gas) }
-                    };
-                    pos = match collides(shaft, rock_type, new_pos) {
-                        true => pos, //Rock would collide with other rocks, so don't move
-                        false => new_pos
-                    };
-                    Mode::Falling
-                }
-                Mode::Falling => {
-                    match pos.1 == 0 {
-                        true => Mode::Stuck, //Bottom of shaft reached
-                        false => {
-                            match pos.1 < shaft.len() + 1 {
-                                true => { //The rock is in range of other rocks, watch out for collisions
-                                    match collides(shaft, rock_type, (pos.0, pos.1 - 1)) {
-                                        true => Mode::Stuck,
-                                        false => {
-                                            pos.1 -= 1;
-                                            Mode::Gas
-                                        }
-                                    }
-                                }
-                                false => { //The rock is not in range of other rocks, so just fall
-                                    pos.1 -= 1;
-                                    Mode::Gas
-                                }
-                            }
-                        }
-                    }
-                }
-                Mode::Stuck => { //Rock has hit something, and is now stuck. Add it to the shaft
-                    rock_type.shape.iter()
-                        .map(|(x, y)| (pos.0 + *x as usize, pos.1 + *y as usize))
-                        .for_each(|(x, y)| {
-                            if shaft.len() <= y {
-                                shaft.push([false; SHAFT_WIDTH]);
-                            }
-                            shaft[y][x] = true;
-                        });
-                    break;
-                }
-            }
-        }
-    }
-}
-
-enum Mode {
-    Falling,
-    Gas,
-    Stuck,
-}
-
-//bottom left is (0,0)
 struct RockType {
     pub shape: Vec<(u8, u8)>,
     pub width: u8,
@@ -156,51 +81,121 @@ impl FromStr for RockType {
     }
 }
 
-fn collides(shaft: &[[bool; SHAFT_WIDTH]], rock_type: &RockType, pos: (usize, usize)) -> bool {
-    rock_type.shape.iter()
-        .map(|(x, y)| (pos.0 + *x as usize, pos.1 + *y as usize))
-        .any(|(x, y)| shaft.get(y).unwrap_or(&[false; SHAFT_WIDTH])[x])
+struct Shaft<'a> {
+    pub shape: Vec<[bool; SHAFT_WIDTH]>,
+    pub rock_types : &'a Vec<RockType>,
+    pub rock_index: usize,
+    pub gas_pattern: &'a Vec<char>,
+    pub gas_index: usize,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct SimState {
-    pub shaft: Vec<[bool; SHAFT_WIDTH]>,
-    pub gas: usize,
-    pub rock: usize,
-}
-
-impl SimState {
-    pub fn new(shaft: &Vec<[bool; SHAFT_WIDTH]>, gas: usize, rock: usize) -> Self {
-        SimState {
-            shaft: minimal_representation_shaft(shaft),
-            gas,
-            rock,
-        }
-    }
-}
-
-fn minimal_representation_shaft(shaft: &Vec<[bool; SHAFT_WIDTH]>) -> Vec<[bool; SHAFT_WIDTH]> {
-    //Only the rows from the top until the row which is fully in the 'shadow' of rocks above are relevant
-    let mut sunlight_reach = [true; SHAFT_WIDTH];
-    let mut cutoff_index = 0;
-
-    for i in (0..shaft.len()).rev() {
-        (0..SHAFT_WIDTH).filter(|x| shaft[i][*x]) //Update sunlight map
-            .for_each(|x| sunlight_reach[x] = false);
-        if sunlight_reach.iter().all(|&x| !x) { //No sunlight left
-            cutoff_index = i;
-            break;
+impl<'a> Shaft<'a> {
+    pub fn new(rock_types: &'a Vec<RockType>, gas_pattern: &'a Vec<char>) -> Self {
+        Shaft {
+            shape: Vec::new(),
+            rock_types,
+            rock_index: 0,
+            gas_pattern,
+            gas_index: 0,
         }
     }
 
-    (cutoff_index..shaft.len()).map(|i| shaft[i]).collect()
-}
+    pub fn simulate(&mut self, n_rocks: usize) {
+        enum Mode { Falling, Gas, Stuck}
 
-fn print_shaft(shaft: &[[bool; SHAFT_WIDTH]]) {
-    for row in shaft.iter().rev() {
-        for col in row.iter() {
-            print!("{}", if *col { '#' } else { '.' });
+        for _ in 0..n_rocks {
+            let rock_type = &self.rock_types[self.rock_index];
+            self.rock_index = (self.rock_index + 1) % self.rock_types.len();
+            let (mut pos, mut mode) = ((2, self.shape.len() + 3), Mode::Gas); //start state
+
+            loop {
+                mode = match mode {
+                    Mode::Gas => {
+                        let gas = &self.gas_pattern[self.gas_index];
+                        self.gas_index = (self.gas_index + 1) % self.gas_pattern.len();
+                        let new_pos = match gas {
+                            '>' => {
+                                match SHAFT_WIDTH > pos.0 + rock_type.width as usize {
+                                    true => (pos.0 + 1, pos.1),
+                                    false => (pos.0, pos.1) //Rock is against the right wall
+                                }
+                            }
+                            '<' => {
+                                match pos.0 > 0 {
+                                    true => (pos.0 - 1, pos.1),
+                                    false => (pos.0, pos.1) //Rock is against the left wall
+                                }
+                            }
+                            _ => { panic!("Invalid gas character: {}", gas) }
+                        };
+                        pos = match self.rock_collides(rock_type, new_pos) {
+                            true => pos, //Rock would collide with other rocks, so don't move
+                            false => new_pos
+                        };
+                        Mode::Falling
+                    }
+                    Mode::Falling => {
+                        match pos.1 == 0 {
+                            true => Mode::Stuck, //Bottom of shaft reached
+                            false => {
+                                match pos.1 < self.shape.len() + 1 {
+                                    true => { //The rock is in range of other rocks, watch out for collisions
+                                        match self.rock_collides(rock_type, (pos.0, pos.1 - 1)) {
+                                            true => Mode::Stuck,
+                                            false => {
+                                                pos.1 -= 1;
+                                                Mode::Gas
+                                            }
+                                        }
+                                    }
+                                    false => { //The rock is not in range of other rocks, so just fall
+                                        pos.1 -= 1;
+                                        Mode::Gas
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Mode::Stuck => { //Rock has hit something, and is now stuck. Add it to the shaft
+                        rock_type.shape.iter()
+                            .map(|(x, y)| (pos.0 + *x as usize, pos.1 + *y as usize))
+                            .for_each(|(x, y)| {
+                                if self.shape.len() <= y {
+                                    self.shape.push([false; SHAFT_WIDTH]);
+                                }
+                                self.shape[y][x] = true;
+                            });
+                        break;
+                    }
+                }
+            }
         }
-        println!();
+    }
+
+    fn rock_collides(&self, rock_type: &RockType, rel_pos: (usize, usize)) -> bool {
+        rock_type.shape.iter()
+            .map(|(x, y)| (rel_pos.0 + *x as usize, rel_pos.1 + *y as usize))
+            .any(|(x, y)| self.shape.get(y).unwrap_or(&[false; SHAFT_WIDTH])[x])
+    }
+
+    pub fn minimal_shape(&self) -> Vec<[bool; SHAFT_WIDTH]>{
+        //Only the rows from the top until the row which is fully in the 'shadow' of rocks above are relevant
+        let mut sunlight_reach = [true; SHAFT_WIDTH];
+        let mut cutoff_index = 0;
+
+        for i in (0..self.shape.len()).rev() {
+            (0..SHAFT_WIDTH).filter(|x| self.shape[i][*x]) //Update sunlight map
+                .for_each(|x| sunlight_reach[x] = false);
+            if sunlight_reach.iter().all(|&x| !x) { //No sunlight left
+                cutoff_index = i;
+                break;
+            }
+        }
+
+        (cutoff_index..self.shape.len()).map(|i| self.shape[i]).collect()
+    }
+
+    pub fn height(&self) -> usize {
+        self.shape.len()
     }
 }
